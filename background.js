f2fee820a8394870f3f64027a8a4234279ca9be6
Pedram Pejman <pedrampejman2010@gmail.com>
2019-07-19 14:56:19 -0700

    Implements basic message passing between content script and extension

--- background.js
+++ background.js
@@ -1,5 +1,22 @@
-'use strict';
+	'use strict';
+
+const ACTION_CAPTURE_PLAYBACK_RATE = 'cation_capture';
+const CAPTURE_PORT_NAME = 'capture_port'
+
+var captureBusPort;
 
 chrome.runtime.onInstalled.addListener(function() {
   console.log("Extension installed");
 });
+
+chrome.runtime.onConnect.addListener(function(port) {
+  console.assert(port.name == CAPTURE_PORT_NAME);
+  captureBusPort = port;
+  port.onMessage.addListener(function(request) {
+  	console.log(request);
+  });
+});
+
+chrome.webNavigation.onCompleted.addListener(function() {
+	captureBusPort.postMessage({action: ACTION_CAPTURE_PLAYBACK_RATE});
+}, {url: [{urlMatches : 'https://www.youtube.com/'}]});
